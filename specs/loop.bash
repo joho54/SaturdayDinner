0f21df4fdff8bac6290dbf57e1c9194aa92acd08
pwerty <pwertyman@naver.com>
2025-06-28 13:36:28 +0900

    feat: spec9~13 모델 제작 완료

--- specs/loop.bash
+++ specs/loop.bash
@@ -1,6 +1,12 @@
-conda deactivate
-conda activate /Users/johyeonho/SaturdayDinner/.conda
-cd ..
-for i in {1..8}; do
+#!/bin/bash
+
+# conda 초기화
+source /opt/homebrew/anaconda3/etc/profile.d/conda.sh
+
+# signlang 환경 활성화
+conda activate signlang
+
+# 현재 디렉토리에서 실행
+for i in {9..13}; do
     python3 main.py specs/spec$i.json
 done
